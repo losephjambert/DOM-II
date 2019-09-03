add3f4f866a266e90fa864dc7c79577447723b53
Joe <josephc.lambert@gmail.com>
2019-09-03 14:54:47 -0700

    adds keydown and mouseover

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const body = document.querySelector('body');
 const h2 = document.querySelectorAll('h2');
 const buttons = document.querySelectorAll('.btn');
 
-// 1. body on scroll behavior
+// 1. scroll
 let lastSeenY = 0;
 body.style.opacity = 1;
 window.addEventListener('scroll', e => {
@@ -19,9 +19,27 @@ window.addEventListener('scroll', e => {
   lastSeenY = currentY <= 0 ? 0 : currentY;
 });
 
-// 2. logo spin
+// 2. click
 logoHeading.addEventListener('click', e => {
   console.log(logoHeading);
   logoHeading.style.transform = `rotate(180deg)`;
   headerImg.style.transform = `translate3d(100%,0,0)`;
 });
+
+// 3. keydown
+body.addEventListener('keydown', e => {
+  if (e.keyCode === 27) {
+    h2.forEach(el => {
+      el.style.transform = `scale(.25)`;
+    });
+  }
+});
+
+// 4. mouseover
+buttons.forEach(button => {
+  button.addEventListener('mouseover', e => {
+    button.style.backgroundColor = 'salmon';
+  });
+});
+
+// 5. resize
