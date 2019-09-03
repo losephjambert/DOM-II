1bf78a0ec645c27067176b730f7ce7e92e9665a9
Joe <josephc.lambert@gmail.com>
2019-09-03 14:49:39 -0700

    handle logo click

--- js/index.js
+++ js/index.js
@@ -9,19 +9,19 @@ const buttons = document.querySelectorAll('.btn');
 // 1. body on scroll behavior
 let lastSeenY = 0;
 body.style.opacity = 1;
-window.addEventListener(
-  'scroll',
-  e => {
-    let currentY = window.pageYOffset || document.documentElement.scrollTop;
-    let scaler = 0;
-    if (currentY > lastSeenY) {
-      scaler += 0.01;
-      body.style.opacity -= 0.01;
-    } else {
-    }
-    lastSeenY = currentY <= 0 ? 0 : currentY;
-  },
-  false
-);
+window.addEventListener('scroll', e => {
+  let currentY = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentY > lastSeenY) {
+    body.style.opacity -= 0.01;
+  } else {
+    body.style.opacity += 0.01;
+  }
+  lastSeenY = currentY <= 0 ? 0 : currentY;
+});
 
 // 2. logo spin
+logoHeading.addEventListener('click', e => {
+  console.log(logoHeading);
+  logoHeading.style.transform = `rotate(180deg)`;
+  headerImg.style.transform = `translate3d(100%,0,0)`;
+});
