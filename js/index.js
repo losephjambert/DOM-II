b804be1ffcfe75bf29bcf9d67f30ad1a2049d036
Joe <josephc.lambert@gmail.com>
2019-09-03 14:39:18 -0700

    handles window scrolling behavior

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+const headerImg = document.querySelector('.intro img');
+const navLinks = document.querySelectorAll('header nav a');
+const body = document.querySelector('body');
+const h2 = document.querySelectorAll('h2');
+const buttons = document.querySelectorAll('.btn');
+
+// 1. body on scroll behavior
+let lastSeenY = 0;
+body.style.opacity = 1;
+window.addEventListener(
+  'scroll',
+  e => {
+    let currentY = window.pageYOffset || document.documentElement.scrollTop;
+    let scaler = 0;
+    if (currentY > lastSeenY) {
+      scaler += 0.01;
+      body.style.opacity -= 0.01;
+    } else {
+    }
+    lastSeenY = currentY <= 0 ? 0 : currentY;
+  },
+  false
+);
+
+// 2. logo spin
